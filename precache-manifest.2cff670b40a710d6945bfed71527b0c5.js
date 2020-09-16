b4cec53aad68b72b24d7ba77b793d69b209371f2
kmobassar <kmobassar@gmail.com>
2020-09-16 21:00:58 +0530

    Updates

---
+++ precache-manifest.2cff670b40a710d6945bfed71527b0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4cea2bf1873a9ed7ca3ebb9c342dcf",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "752e49c05feda3b7250b",
+    "url": "/netflix-clone/static/css/main.cbc77a27.chunk.css"
+  },
+  {
+    "revision": "a7f3cf3538b74363cc17",
+    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752e49c05feda3b7250b",
+    "url": "/netflix-clone/static/js/main.5c004e53.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
